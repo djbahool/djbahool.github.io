57a16b969f3ae5e064ccdcdcf59bddcaf0e0cff7
djbahool <66438242+djbahool@users.noreply.github.com>
2020-06-06 07:52:26 +0200

    start button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 console.log("js running");
 $('button').toggleClass('blue');
-function f1() {
-    $('button').html(0);
+function startGame() {
+    $('#start').html('Re-Start');
 }
-$('button').click(f1);
+$('#start').click(startGame);
 
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
