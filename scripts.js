020e727f415aa9662a358dc15172b48aa56069bb
djbahool <66438242+djbahool@users.noreply.github.com>
2020-06-06 07:08:40 +0200

    buttons

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("js running");
+$('button').toggleClass('blue');
+function f1() {
+    $('button').html(0);
+}
+$('button').click(f1);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
