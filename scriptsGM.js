3ce0f9a42dc8a23bc3f9176fbec37afe99a2834b
djbahool <66438242+djbahool@users.noreply.github.com>
2020-06-08 09:24:59 +0200

    2

--- scriptsGM.js
+++ scriptsGM.js
@@ -2,8 +2,15 @@ console.log("JS is loaded!");
 $('button').toggleClass('blue');
 function startGame() {
     $('#start').html('Re-Start');
+    $('.btTTT').html('');
+}
+function putX() {
+    if ($('.btTTT').html=='') {
+        $('.btTTT').html="X";
+    }
 }
 $('#start').click(startGame);
+$('btTTT').click(putX);
 
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
